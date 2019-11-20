3872d0be8f75ad35c9fba28dc295168aeca1a349
Lajanugen Logeswaran <llajan@deeplearn20.eecs.umich.edu>
2019-11-20 06:36:41 -0500

    Minor updates

--- scripts/eval_all.sh
+++
@@ -1,2 +0,0 @@
-# Feed-forward model, LM training
-./scripts/run_lm.sh -m task_agnostic --mdl default -f --max-epoch 50 -i ff_taskagnostic/checkpoint10.pt --eval --no-training
--- scripts/train_all.sh
+++
@@ -1,2 +0,0 @@
-# Feed-forward model, LM training
-./scripts/run_lm.sh -e ff_taskagnostic -m task_agnostic --mdl default --tb --max-epoch 10
